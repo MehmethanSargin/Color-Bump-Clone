0e02197b3debf03ebdd1fee5a92dac39505743dd
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-28 22:34:25 +0300

    feat(camera): add camera follow #.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed = 6;
+    [SerializeField]
+    public Vector3 camVelocity;
+
+    private void Update()
+    {
+        transform.position += Vector3.forward * cameraSpeed * Time.deltaTime;
+        camVelocity = Vector3.forward * cameraSpeed * Time.deltaTime;
+    }
+}
